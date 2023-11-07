cd6b521ce1305a086a42f9c59f93ed44c4cb6aae
vanshika105 <vanshikakush.dev@gmail.com>
2023-11-07 23:24:20 +0530

    Modified code

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,47 +1,54 @@
-import { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { useSelector } from "react-redux";
 
 const Details = () => {
   const [clear, setClear] = useState(false);
-  const { country, state, places, abbr } = useSelector(
-    (state) => state.zipInfo
-  );
-  
-  function clearData(){
-    setClear(() => !clear);
-  }
+  const { country, state, places, abbr } = useSelector((state) => state.zipInfo);
+
+  const clearData = useCallback(() => {
+    setClear((prevClear) => !prevClear);
+  }, []);
 
   return (
     <>
-    {!clear && <div className="flex flex-cols items-center justify-between px-5 mt-16">
-      <div className="flex flex-rows gap-4 items-center">
-        <div className="text-left">
-          <img
-            className="ml-2"
-            src={`https://flagcdn.com/${abbr}.svg`}
-            width="40"
-            alt="Ukraine"
-          />
-          <p className="text-emerald-900 text-3xl font-medium mt-4">{country}</p>
-          <p className="text-darker-green text-lg font-normal mt-1">{state}</p>
-        </div>
-      </div>
-      <div className="flex flex-rows gap-4 items-center">
-        <div className="text-left">
-          <div className="text-emerald-900 font-medium text-2xl mb-4">
-            Places
+      {!clear && (
+        <div className="flex flex-cols items-center justify-between px-5 mt-16">
+          <div className="flex flex-rows gap-4 items-center">
+            <div className="text-left">
+              <img
+                className="ml-2"
+                src={`https://flagcdn.com/${abbr}.svg`}
+                width="40"
+                alt={country}
+              />
+              <p className="text-emerald-900 text-3xl font-medium mt-4">
+                {country}
+              </p>
+              <p className="text-darker-green text-lg font-normal mt-1">{state}</p>
+            </div>
           </div>
-          <ul className="text-darker-green font-medium list-disc ml-6">
-            {places.map((item) => (
-              <li>{item}</li>
-            ))}
-          </ul>
+          <div className="flex flex-rows gap-4 items-center">
+            <div className="text-left">
+              <div className="text-emerald-900 font-medium text-2xl mb-4">
+                Places
+              </div>
+              <ul className="text-darker-green font-medium list-disc ml-6">
+                {places.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
+      )}
+      <div className="mt-6">
+        <button
+          className="text-lg text-green-bg mt-8 bg-darker-green rounded-lg px-3 py-1"
+          onClick={clearData}
+        >
+          {clear ? "Display" : "Clear"}
+        </button>
       </div>
-    </div>}
-    <div className="mt-6">
-    <button className="text-lg text-green-bg mt-8 bg-darker-green rounded-lg px-3 py-1" onClick={clearData}>{clear ? "Display" : "Clear"}</button>
-    </div>
     </>
   );
 };
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,12 @@
-import { fetchZipData } from "../store/zip-actions";
-import { useDispatch } from 'react-redux';
-import { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { FiSearch } from "react-icons/fi";
 import { IoMdClose } from "react-icons/io";
+import { useDispatch } from "react-redux";
+import { fetchZipData } from "../store/zip-actions";
 
 const SearchBar = ({ bgColor, width, placeholder }) => {
   const [isFocused, setIsFocused] = useState(false);
-  const [enteredZip, setEnteredZip] = useState(''); 
+  const [enteredZip, setEnteredZip] = useState("");
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
 
@@ -23,40 +23,53 @@ const SearchBar = ({ bgColor, width, placeholder }) => {
     setEnteredZip("");
   };
 
-  const handleInputChange = (event) => {
-    if (event.key === 'Enter') {
-      setLoading(true);
-      dispatch(fetchZipData(enteredZip))
-        .then(() => {
-          setLoading(false);
-        })
-    } else {
-      setEnteredZip(event.target.value);
-    }
-  }
+  const handleInputChange = useCallback(
+    (event) => {
+      const newEnteredZip = event.target.value;
+      setEnteredZip(newEnteredZip);
+
+      if (event.key === "Enter") {
+        setLoading(true);
+        dispatch(fetchZipData(newEnteredZip))
+          .then(() => {
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error("Error fetching zip data:", error);
+            setLoading(false);
+          });
+      }
+    },
+    [dispatch]
+  );
 
   return (
     <>
-      <div className={`flex rounded-lg ${bgColor} shadow-inner relative ${width} mt-6`}>
-      <span className="p-2 text-emerald-900 text-lg">
-        <FiSearch />
-      </span>
-      <input
-        className={`${bgColor} text-11 w-60 rounded-md placeholder-darker-green focus:outline-none`}
-        placeholder={placeholder}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        value={enteredZip}
-        onChange={handleInputChange}
-        onKeyDown={handleInputChange}
-      />
-      {isFocused && (
-        <span className="p-2 text-emerald-900 text-lg absolute right-0 cursor-pointer" onClick={handleClearClick}>
-          <IoMdClose />
+      <div
+        className={`flex rounded-lg ${bgColor} shadow-inner relative ${width} mt-6`}
+      >
+        <span className="p-2 text-emerald-900 text-lg">
+          <FiSearch />
         </span>
-      )}
-    </div>
-    {loading && <div>Loading data...</div>}
+        <input
+          className={`${bgColor} text-11 w-60 rounded-md placeholder-darker-green focus:outline-none`}
+          placeholder={placeholder}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          value={enteredZip}
+          onChange={handleInputChange}
+          onKeyDown={handleInputChange}
+        />
+        {isFocused && (
+          <span
+            className="p-2 text-emerald-900 text-lg absolute right-0 cursor-pointer"
+            onClick={handleClearClick}
+          >
+            <IoMdClose />
+          </span>
+        )}
+      </div>
+      {loading && <div>Loading data...</div>}
     </>
   );
 };
